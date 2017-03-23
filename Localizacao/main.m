c89fb33d3ac3310dfa7dd8a450e00efba308dcc8
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2017-03-23 16:06:38 -0300

    Initial Commit

---
+++ Localizacao/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Localizacao
+//
+//  Created by Eduardo Vital Alencar Cunha on 23/03/17.
+//  Copyright © 2017 Vital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
